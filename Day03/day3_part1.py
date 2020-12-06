d019cbea167d56c4270b1f8ad3d87d551eefd223
Brenden Hyde <brendenahyde@gmail.com>
2020-12-06 12:47:23 -0800

    progress for day 3, part 1

--- Day03/day3_part1.py
+++ Day03/day3_part1.py
@@ -1,3 +1,4 @@
+import math
 import sys
 
 
@@ -27,7 +28,24 @@ def find_min_grid_width(rows):
 def construct_repeated_grid(grid, min_width):
     '''Given a list of strings representing grid rows, return a list of strings where each row is repeated
        enough times to support a minimum width of `min_width`.'''
-    ...
+    current_width = len(grid[0])
+    print(f'The current width of the grid is {current_width}')
+    print(f'In order to support right-3, down-1 traversal, the grid needs to be {min_width} characters wide')
+    repeats = math.ceil(min_width / current_width)
+    print(f'We need to repeat the grid from left to right {repeats} times')
+
+    return [row * repeats for row in grid]
+
+
+def test_grid_construction(test_grid, rep_grid):
+    '''Given the name of a file `test_grid`, read it and compare it to a repeated grid constructed by
+       the `construct_repeated_grid` function.
+       Return True if the grids are the same, otherwise False
+
+       The purpose of this test is to make sure that my repeated grid looks like the one from the example'''
+
+    test_grid = read_input(test_grid)
+    return test_grid == rep_grid
 
 
 def main():
@@ -38,7 +56,9 @@ def main():
 
     rows = len(grid)
     min_width = find_min_grid_width(rows)
-    print(f'The minimum grid width is: {min_width}')
+    print(min_width)
+
+    rep_grid = construct_repeated_grid(grid, min_width)
 
 
 if __name__ == '__main__':
