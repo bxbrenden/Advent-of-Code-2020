8e8a72cb1c4104580232bca075367806c1344bbb
Brenden Hyde <brendenahyde@gmail.com>
2020-12-06 17:57:47 -0800

    Correct answer for day 4, part 2

--- Day04/day4_part2.py
+++ Day04/day4_part2.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+import re
 import sys
 
 
@@ -65,7 +66,7 @@ def valid_byr(byr):
 def valid_iyr(iyr):
     '''Validate the issue year: must be 4 digits and between 2010 and 2020 inclusive'''
     try:
-        byr = int(iyr)
+        iyr = int(iyr)
         if len(str(iyr)) == 4:
             if iyr >= 2010 and iyr <= 2020:
                 return True
@@ -92,6 +93,79 @@ def valid_eyr(eyr):
         return False
 
 
+def valid_hgt(hgt):
+    '''Validate the height: must be a number followed by "in" or "cm"
+       - If "cm", must be between 150 and 193 inclusive
+       - If "in", must be between 59 and 76 inclusive'''
+
+    def find_delimiter(hgt):
+        '''Find where the integers stop and the characters start.
+           Return the index of the first char.'''
+
+        for index, char in enumerate(hgt):
+            try:
+                num = int(char)
+            except ValueError:
+                return index
+
+        return None
+
+    if index := find_delimiter(hgt):
+        num = int(hgt[:index])
+        units = hgt[index:]
+        #print(f'Number: {num}, Units: {units}')
+
+        if units == "cm" or units == "in":
+            if units == "cm":
+                if num >= 150 and num <=193:
+                    return True
+                else:
+                    return False
+            elif units == "in":
+                if num >= 59 and num <= 76:
+                    return True
+                else:
+                    return False
+        else:
+            return False
+    else:
+        return False
+
+
+def valid_hcl(hcl):
+    '''Validate hair color: must begin with "#" followed by 6 chars [0-9a-f]'''
+    hcl_regex = r'^#[0-9a-f]{6}'
+    hcl_pattern = re.compile(hcl_regex)
+    if hcl_pattern.match(hcl):
+        return True
+    else:
+        return False
+
+
+def valid_ecl(ecl):
+    '''Validate the eye color: must be in set of ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']'''
+    eye_colors = set(['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'])
+    if ecl in eye_colors:
+        return True
+    else:
+        return False
+
+
+def valid_pid(pid):
+    '''Validate the passport ID: must be 9 digits long, all numerals [0-9], and may have leading zeroes'''
+
+    if len(pid) == 9:
+        for char in pid:
+            try:
+                int(char)
+            except:
+                return False
+    else:
+        return False
+
+    return True
+
+
 def validate_passports(passport_list):
     '''Given a list of all the unmodified passport strings (newlines intact), do the following:
        - replace all newlines with spaces
@@ -102,9 +176,24 @@ def validate_passports(passport_list):
     valid = 0
     for passport in passport_list:
         passport = newline_to_space(passport)
-        passport_dict = passport_str_to_dict(passport)
-        if is_valid := has_all_mandatory_keys(passport_dict):
-            valid += 1
+        pass_dict = passport_str_to_dict(passport)
+        if has_mandos := has_all_mandatory_keys(pass_dict):
+            byr = pass_dict['byr']
+            iyr = pass_dict['iyr']
+            eyr = pass_dict['eyr']
+            hgt = pass_dict['hgt']
+            hcl = pass_dict['hcl']
+            ecl = pass_dict['ecl']
+            pid = pass_dict['pid']
+
+            if valid_byr(byr):
+                if valid_iyr(iyr):
+                    if valid_eyr(eyr):
+                        if valid_hgt(hgt):
+                            if valid_hcl(hcl):
+                                if valid_ecl(ecl):
+                                    if valid_pid(pid):
+                                        valid += 1
 
     return valid
 
