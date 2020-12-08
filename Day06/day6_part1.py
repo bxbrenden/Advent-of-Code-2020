0928695bcbb17f8004aee3ccc14e28449b514327
Brenden Hyde <brenden.hyde@autodesk.com>
2020-12-07 17:37:56 -0800

    Correct answer for day 6, part 1

--- Day06/day6_part1.py
+++ Day06/day6_part1.py
@@ -13,12 +13,36 @@ def read_input(input_file):
         sys.exit(1)
 
 
+def entry_to_set(entry):
+    '''Given an entry from the list of filled out questionnaires, turn them into a set of unique, lowercase chars and return it'''
+
+    # First check if there are any newlines in the entry string
+    if '\n' in entry:
+        new_entry = set(''.join([x for x in entry.split('\n')]))
+    else:
+        new_entry = set(''.join([x for x in entry]))
+
+    print(f'Converted entry {entry} into new entry {new_entry}')
+    return new_entry
+
+
+def sum_list_of_sets(char_sets):
+    '''Using a list composed of the outputs of `entry_to_set`, return the sum of the lengths of all sets'''
+    return sum([len(cs) for cs in char_sets])
+
+
 def main():
     input_file = 'input.txt'
     questions = read_input(input_file)
 
+    char_sets = []
     for q in questions:
-        print(q)
+        char_set = entry_to_set(q)
+        char_sets.append(char_set)
+
+    sum_of_set_lengths = sum_list_of_sets(char_sets)
+    print(f'ANSWER: the sum of the lengths of all character sets is {sum_of_set_lengths}')
+
 
 
 if __name__ == '__main__':
