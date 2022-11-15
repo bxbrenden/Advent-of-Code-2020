7a9b88935b1661a2f759f4607e83e1b70fe43fdf
Brenden Hyde <brendenahyde@gmail.com>
2022-11-14 22:27:17 -0800

    fix indentation

--- Day01/day01-part2.c
+++ Day01/day01-part2.c
@@ -30,49 +30,49 @@ void get_int_vals(char *filename, int *vals, int num_newlines);
 int solve_puzzle(int *vals, int num_newlines);
 
 int main(int argc, char *argv[]) {
-	FILE *input = fopen(PUZZLE_INPUT, "r");
-	int num_newlines = count_newlines(input);
-	int vals[num_newlines];
-	int answer;
-	get_int_vals(PUZZLE_INPUT, &vals[0], num_newlines);
-	answer = solve_puzzle(&vals[0], num_newlines);
-	printf("The answer is %d\n", answer);
-	return 0;
+  FILE *input = fopen(PUZZLE_INPUT, "r");
+  int num_newlines = count_newlines(input);
+  int vals[num_newlines];
+  int answer;
+  get_int_vals(PUZZLE_INPUT, &vals[0], num_newlines);
+  answer = solve_puzzle(&vals[0], num_newlines);
+  printf("The answer is %d\n", answer);
+  return 0;
 }
 
 int count_newlines(FILE *inp) {
-	int num_newlines = 0;
-	char line[LINE_LEN];
+  int num_newlines = 0;
+  char line[LINE_LEN];
 
-	while (fgets(line, LINE_LEN, inp)) {
-		num_newlines++;
-	}
+  while (fgets(line, LINE_LEN, inp)) {
+    num_newlines++;
+  }
 
-	return num_newlines;
+  return num_newlines;
 }
 
 void get_int_vals(char *filename, int *vals, int num_newlines) {
-	FILE * inp = fopen(filename, "r");
-	char line[LINE_LEN];
+  FILE * inp = fopen(filename, "r");
+  char line[LINE_LEN];
 
-	for (int i=0; fgets(line, LINE_LEN, inp); i++) {
-		int current_val = atoi(line);
-		vals[i] = current_val;
-	}
+  for (int i=0; fgets(line, LINE_LEN, inp); i++) {
+    int current_val = atoi(line);
+    vals[i] = current_val;
+  }
 }
 
 int solve_puzzle(int *vals, int num_newlines) {
-	int outer, inner, thinner;
+  int outer, inner, thinner;
 
-	for (outer=0; outer<num_newlines; outer++) {
-		for (inner=0; inner<num_newlines; inner++) {
-			for (thinner=0; thinner<num_newlines; thinner++) {
-				if (vals[outer] + vals[inner] + vals[thinner] == 2020) {
-					printf("%d + %d + %d == 2020\n", vals[outer], vals[inner], vals[thinner]);
-					return vals[outer] * vals[inner] * vals[thinner];
-				}
-			}
-		}
-	}
-	return 0;
+  for (outer=0; outer<num_newlines; outer++) {
+    for (inner=0; inner<num_newlines; inner++) {
+      for (thinner=0; thinner<num_newlines; thinner++) {
+        if (vals[outer] + vals[inner] + vals[thinner] == 2020) {
+          printf("%d + %d + %d == 2020\n", vals[outer], vals[inner], vals[thinner]);
+          return vals[outer] * vals[inner] * vals[thinner];
+        }
+      }
+    }
+  }
+  return 0;
 }
